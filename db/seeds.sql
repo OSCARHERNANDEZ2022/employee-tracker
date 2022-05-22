92b617e10800dccc88ed856cfbe1a99309a8d67e
Oscar Hernandez <oscarhernandezmex@gmail.com>
2022-05-22 13:58:52 -0700

    db folder files added

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+('Oscar', 'Hernandez', 4,1),
+('David', 'Campos', 1,4),
+('Emiliano', 'Zapata', 1, 4),
+('Samantha', 'Davidson', 2,4),
+('Mike', 'Nielson', 3, 4);
